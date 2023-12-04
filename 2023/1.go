5002fe485753ea859d8c8ea6e92154db364943dc
pd <xpd@duck.com>
2023-12-04 18:59:33 +0530

    day1p2

--- 2023/1.go
+++ 2023/1.go
@@ -7,32 +7,6 @@ import (
 	"strings"
 )
 
-func getFirst(x string) int {
-	var charInt int
-	for i := 0; i < len(x); i++ {
-		char := string(x[i])
-		if isInt(char) {
-			charInt, _ = strconv.Atoi(char)
-			break
-		}
-
-	}
-	return charInt
-}
-
-func getLast(x string) int {
-	var charInt int
-	for i := 1; i <= len(x); i++ {
-		char := string(x[len(x)-i])
-		if isInt(char) {
-			charInt, _ = strconv.Atoi(char)
-			break
-		}
-
-	}
-	return charInt
-}
-
 func isInt(x string) bool {
 	_, err := strconv.Atoi(x)
 	if err != nil {
@@ -41,10 +15,71 @@ func isInt(x string) bool {
 	return true
 }
 
-func getNum(x, y int) int {
-	strNum := strconv.Itoa(x) + strconv.Itoa(y)
+func getNum(x, y string) int {
+	strNum := x + y
 	num, _ := strconv.Atoi(strNum)
 	return num
+}
+
+func getFirst(x string) string {
+	str := strings.ToLower(x)
+	for i := 0; i < len(str); i++ {
+		switch {
+		case isInt(string(str[i])):
+			return string(str[i])
+		case len(str) >= i+3 && str[i:i+3] == "one":
+			return "1"
+		case len(str) >= i+3 && str[i:i+3] == "two":
+			return "2"
+		case len(str) >= i+5 && str[i:i+5] == "three":
+			return "3"
+		case len(str) >= i+4 && str[i:i+4] == "four":
+			return "4"
+		case len(str) >= i+4 && str[i:i+4] == "five":
+			return "5"
+		case len(str) >= i+3 && str[i:i+3] == "six":
+			return "6"
+		case len(str) >= i+5 && str[i:i+5] == "seven":
+			return "7"
+		case len(str) >= i+5 && str[i:i+5] == "eight":
+			return "8"
+		case len(str) >= i+4 && str[i:i+4] == "nine":
+			return "9"
+
+		}
+	}
+	return ""
+}
+
+func getLast(x string) string {
+	str := strings.ToLower(x)
+	for i := 0; i < len(str); i++ {
+		j := len(str) - i
+		switch {
+		case isInt(string(str[j-1])):
+			return string(str[j-1])
+		case j-3 >= 0 && str[j-3:j] == "one":
+			return "1"
+		case j-3 >= 0 && str[j-3:j] == "two":
+			return "2"
+		case j-5 >= 0 && str[j-5:j] == "three":
+			return "3"
+		case j-4 >= 0 && str[j-4:j] == "four":
+			return "4"
+		case j-4 >= 0 && str[j-4:j] == "five":
+			return "5"
+		case j-3 >= 0 && str[j-3:j] == "six":
+			return "6"
+		case j-5 >= 0 && str[j-5:j] == "seven":
+			return "7"
+		case j-5 >= 0 && str[j-5:j] == "eight":
+			return "8"
+		case j-4 >= 0 && str[j-4:j] == "nine":
+			return "9"
+
+		}
+	}
+	return ""
 }
 
 func main() {
@@ -54,14 +89,14 @@ func main() {
 	}
 
 	fileSlice := strings.Split(string(fo), "\n")
-	fmt.Println(fileSlice)
 	var totalSum int
 	for i := 0; i < len(fileSlice); i++ {
-		wslice := fileSlice[i]
+		wlslice := fileSlice[i]
 
-		num := getNum(getFirst(wslice), getLast(wslice))
+		num := getNum(getFirst(wlslice), getLast(wlslice))
+		fmt.Println(wlslice, ":", num)
 		totalSum += num
-		fmt.Println(wslice, "", num)
+
 	}
 	fmt.Println(totalSum)
 }
